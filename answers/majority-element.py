5eb1f6b6df5ea24510449090d8c60bfe77ae91cc
Sam <sam@wanfamily.org>
2024-10-02 12:07:44 -0700

    10/2/24 2

---
+++ answers/majority-element.py
@@ -0,0 +1,8 @@
+def majorityElement(self, nums: List[int]) -> int:
+    #linear time, constant space
+    count = 0
+    candidate = None
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += (1 if num == candidate else -1)
