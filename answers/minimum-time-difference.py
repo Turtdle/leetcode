555ac625a2343a0aa7965525c24d4c6fb0e64823
Sam <sam@wanfamily.org>
2024-10-22 20:55:32 -0700

    10/22/24

--- answers/minimum-time-difference.py
+++ answers/minimum-time-difference.py
@@ -1,5 +1,3 @@
 def findMinDifference(self, timePoints: List[str]) -> int:
     timePoints = sorted([int(time[:2]) * 60 + int(time[3:]) for time in timePoints])
-    timePoints.append(timePoints[0] + 1440)
-    return min(timePoints[i + 1] - timePoints[i] for i in range(len(timePoints) - 1)
-               for i in range(len(timePoints) - 1))
+        
---
+++ answers/search-in-rotated-sorted-array.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+        
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+
+def main():
+    test = [3,4,5,6,1,2]
+    target = 2
+    s = Solution()
+    print(s.search(test, target))
+        
+if __name__ == "__main__":
+    main()
+
+                
